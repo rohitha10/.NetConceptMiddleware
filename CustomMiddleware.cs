b68af3f07e52d01449d3f52f1aa0cae308b50fa5
rohitha10 <chennarohitha1098@gmail.com>
2020-05-22 13:33:38 +0530

    first commit

---
+++ CustomMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Middleware1
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public CustomMiddleware(RequestDelegate next,ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger("Custom Middleware");
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            _logger.LogInformation("CustomMiddleware executing..");
+            await _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomMiddleware>();
+        }
+    }
+}
